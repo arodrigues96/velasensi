f00f482913d26acf6fdda1306764ecab4f20ee86
arodrigues96 <128206548+arodrigues96@users.noreply.github.com>
2024-11-09 11:31:17 -0800

    Update Brand.js

--- src/components/Brand/Brand.js
+++ src/components/Brand/Brand.js
@@ -10,7 +10,7 @@ const Brand = (props) => {
       role={'presentation'}
       onClick={() => navigate('/')}
     >
-      {/* <h4>SENSI VELAS</h4> */}
+      {* <h4>SENSI VELAS</h4> *}
       <svg
         width={127}
         height={24}
