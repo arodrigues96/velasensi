4c14577379a2253ed8f88e17cd3a0012f9b46390
arodrigues96 <128206548+arodrigues96@users.noreply.github.com>
2024-11-09 13:10:58 -0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,8 +31,8 @@ const IndexPage = () => {
       <Hero
         maxWidth={'500px'}
         image={'/banner1.png'}
-        title={'Essentials for a cold winter'}
-        subtitle={'Discover Autumn Winter 2021'}
+        title={'BLABLA SENSI'}
+        subtitle={'VAI NEYMAR THE GOAT!'}
         ctaText={'shop now'}
         ctaAction={goToShop}
       />
